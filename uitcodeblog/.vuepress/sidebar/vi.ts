b878426f3f419e759cefa5b7353508c1d21b4751
HoangThien UITCODE <hoangthien.uit@gmail.com>
2022-09-25 18:28:18 +0700

    [ADD] Init blog

---
+++ uitcodeblog/.vuepress/sidebar/vi.ts
@@ -0,0 +1,31 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export const vi = sidebar({
+  "/": [
+    "",
+    // {
+    //   icon: "creative",
+    //   text: "Guide",
+    //   prefix: "guide/",
+    //   link: "guide/",
+    //   children: "structure",
+    // },
+    {
+      text: "Lập trình web",
+      icon: "note",
+      prefix: "blog/",
+      children: "structure",
+      // children: [
+      //   {
+      //     text: "Drupal Development",
+      //     icon: "note",
+      //     collapsable: true,
+      //     children: [
+      //       "services-and-dependency-injection",
+      //       "dependency-injection-in-controller"
+      //     ],
+      //   },
+      // ],
+    },
+  ],
+});
